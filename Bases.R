06443bc940455e6f0167750fcf1f4104d3dd97ce
Roderictus <rodrigofrancofuentes@gmail.com>
2017-12-28 09:31:40 -0600

    graficas descriptivos ENOE Jal y Nal

--- Bases.R
+++ Bases.R
@@ -266,8 +266,88 @@ MunMapJal2015<-left_join(x = MunMapJal, y = JALMUNMUN2015, by = "NOM_MUN_JOIN")#
 write.csv(x = MunMapJal2015, file = "Datos/Electorales/Jalisco/MunMapJal2015.csv")
 
 #####################################################################################################
-##############      ENOE a nivel municipal      #####################################
+##############      ENOE a nivel municipal      ####################################################
 #####################################################################################################
+
+# De sdem105.dbf a sdem416.dbf
+ENOE <- read.dbf(file = "Datos/ENOE/Sociodemografico/sdemt215.dbf") #  2do trimestre 2015 
+
+
+
+ENOE <- filter(ENOE, R_DEF == "00") #00 Resultado definitivo de la entrevista, entrevista completa
+ENOE$EDA<-as.numeric(ENOE$EDA)
+ENOE <- filter(ENOE, EDA >= 14)
+ENOE <- filter(ENOE, EDA <=98)
+ENOE <- filter(ENOE, C_RES == 1 | C_RES == 3) #Condiciónn de la residencia, 1 Residente Habitual, 3 Nuevo residente 
+# Nuevas variables
+ENOE<- mutate(ENOE, INF = ifelse(TUE2==5,1,0))
+ENOE<- mutate(ENOE, INF_POB = INF * FAC)
+ENOE<- mutate(ENOE, Estado = as.factor(ENT))
+#nombres de los Estados
+ENOE$Estado<- factor(ENOE$Estado, labels = c("AGS", "BC", "BCS","CAMP", "COAH", "COL", "CHIS", "CHIH", "DF", "DGO", "GTO","GRO", "HGO", "JAL", "MEX","MICH", "MOR", "NAY", "NL", "OAX","PUE", "QRO", "QR", "SLP", "SIN", "SON", "TAB", "TAMS", "TLAX", "VER", "YUC", "ZAC"))
+ENOE$SEX<- factor(ENOE$SEX, labels = c("HOMBRE","MUJER")) #Sexo
+ENOE$TUE2<- factor(ENOE$TUE2, labels = c("No Clasif","EmpSocCop","Neg. noConst", "Priv", "Pub", "Sect. Informal", "TDR", "AgSubs")) 
+ENOE$RAMA_EST2<- factor(ENOE$RAMA_EST2, labels = c("No Clasif","Agricultura GSCP","Extractiva y Electricidad", "Manufactura", "Construcción", "Comercio", "Restaurantes y ServAloj", "Transportes y Com...", "Serv. Prof", "Serv. Sociales", "Sevr. Diversos", "Gobierno y Org. Int.")) 
+ENOE$E_CON <- factor(ENOE$E_CON, labels = c ("Unión libre", "Separado(a)", "Divorciado(a)", "Viudo(a)", "Casado(a)", "Soltero(a)", "No sabe"))
+ENOE$CLASE1 <- factor(ENOE$CLASE1, labels = c( "PEA", "No PEA" ))
+ENOE$ING7C<- factor(ENOE$ING7C, labels =c("No Clas","Hasta 1 SM", "1-2 SM", "2-3 SM", "3-5 SM", "Más de 5 SM", "No recibe ingresos", "No especificado"))  #clasificación de la población ocupada por nivel de ingreso 
+ENOE$MEDICA5C <- factor(ENOE$MEDICA5C, labels = c("No Clas", "Sin prestaciones", "Solo acceso a instituciones de salud", "Acceso a inst. de salud y otras prest.", "no tiene acceso, pero si otras prestaciones", "No especificado" ))
+ENOE$AMBITO2 <- factor (ENOE$AMBITO2, labels = c("no clas", "Sin Establecimiento" , "Con establecimiento", "Pequeños establecimientos" , "Medianos Establecimientos", "Grandes Establecimientos", "Gobierno", "Otros") )
+ENOE<- mutate(ENOE, Mujer = ifelse(SEX == "MUJER",1,0)) #Mujeres
+ENOE<- mutate(ENOE, Mujer_Pob = FAC * Mujer) #Mujeres multiplicadas por el factor de poblacion
+ENOE<- mutate(ENOE, Hombre = ifelse(SEX == "HOMBRE",1,0)) #Hombres
+ENOE<- mutate(ENOE, Hombre_Pob = FAC * Hombre) #Hombres multiplicados por el factor de poblacion
+ENOE<- mutate(ENOE, ING_Pob_HR = ING_X_HRS * FAC)
+ENOE<- mutate(ENOE, OCUPADA = ifelse(CLASE2 ==1,1,0)) #Poblacion ocupada, para sacar ingresos promedio
+ENOE<- mutate(ENOE, OCUPADA_Pob = OCUPADA * FAC) #Poblacion ocupada, para sacar ingresos promedio
+ENOE<- mutate(ENOE, HRSOCUP_Pob = HRSOCUP * FAC)
+ENOE<- mutate(ENOE, ANIOS_ESC_Pob = ANIOS_ESC * FAC)
+ENOE<- mutate(ENOE, SUB_O_POB = SUB_O * FAC)
+#Codificación de las variables de escolaridad
+ENOE$CS_P13_1<- factor(ENOE$CS_P13_1, labels = c("Ninguna", "Preescolar", "Primaria", "Secundaria","Preparatoria o bach", "Normal", "Carrera técnica", "Profesional", "Maestría", "Doctorado", "No sabe"))
+#Para cada nivel de escolaridad
+ENOE$Ninguna<-ifelse(ENOE$CS_P13_1 == "Ninguna",1,0) #Ninguna
+ENOE$Ninguna<-ENOE$Ninguna * ENOE$FAC
+ENOE$Preescolar<-ifelse(ENOE$CS_P13_1 == "Preescolar",1,0)
+ENOE$Preescolar<-ENOE$Preescolar * ENOE$FAC
+ENOE$Primaria<-ifelse(ENOE$CS_P13_1 == "Primaria",1,0)
+ENOE$Primaria<-ENOE$Primaria * ENOE$FAC
+ENOE$Secundaria<-ifelse(ENOE$CS_P13_1 == "Secundaria",1,0)
+ENOE$Secundaria<-ENOE$Secundaria * ENOE$FAC
+ENOE$Preparatoria<-ifelse(ENOE$CS_P13_1 == "Preparatoria o bach",1,0)
+ENOE$Preparatoria<-ENOE$Preparatoria * ENOE$FAC
+ENOE$Normal<-ifelse(ENOE$CS_P13_1 == "Normal",1,0)
+ENOE$Normal<-ENOE$Normal * ENOE$FAC
+ENOE$Tecnica<-ifelse(ENOE$CS_P13_1 == "Carrera técnica",1,0)
+ENOE$Tecnica<-ENOE$Tecnica * ENOE$FAC
+ENOE$Profesional<-ifelse(ENOE$CS_P13_1 == "Profesional",1,0)
+ENOE$Profesional<-ENOE$Profesional * ENOE$FAC
+ENOE$Maestria<-ifelse(ENOE$CS_P13_1 == "Maestría",1,0)
+ENOE$Maestria<-ENOE$Maestria * ENOE$FAC
+ENOE$Doctorado<-ifelse(ENOE$CS_P13_1 == "Doctorado",1,0)
+ENOE$Doctorado<-ENOE$Doctorado * ENOE$FAC
+ENOE$NS<-ifelse(ENOE$CS_P13_1 == "No sabe",1,0)
+ENOE$NS<-ENOE$NS * ENOE$FAC
+##### Para las gráficas 
+ENOEB <- ENOE[ENOE$EDA <=75,] #ENOEB restringe la edad de 14 a 75 años para comparación de ingreso salarial 
+ENOEB <- ENOEB[ENOEB$INGOCUP >0,]#ojo
+#####
+IngresoOcup <- ggplot(ENOEB, aes(EDA, INGOCUP, color = SEX)) #edad, Ingreso mensual, sexo, todas las escolaridades 
+IngresoOcup <- IngresoOcup + geom_smooth(aes(weight = FAC))
+IngresoOcup <- IngresoOcup + xlab("Edad (14 a 75 años)") +ylab("Ingreso Mensual")  + ggtitle("Ingreso Mensual declarado por  Sexo(ENOE, II 2015) ")
+IngresoOcup + facet_wrap(~Estado)
+IngresoOcup + facet_wrap(~CS_P13_1) + geom_jitter(alpha = 0.01) + coord_cartesian( xlim = c(0, 75), ylim = c(0, 25000))
+
+head(ENOE)
+
+
+
+
+#####################################################################################################
+##############      Intercensal     #################################################################
+#####################################################################################################
+
+
 
 #shapefiles con formas estatales
 #agregar datos a nivel estatal
--- Capitulos/Por hacer.R
+++ Capitulos/Por hacer.R
@@ -3,18 +3,9 @@ Por hacer:
   #2. Checar que empaten los nombres de los Municipios
   3. Mapa de participación municipal en quintiles. 
   #4. Reemplazar lectura de bases en internet con bases locales 
-  5. Añadir datos municipales, demográficos de ENOE, intercensal
-  6. Homologar nombres
+  #6. Homologar nombres
   7. Empatar por Estado-Nombre (En lugar de clave única)
-  
-  for ( i in 1:32) {
-    A2 <- dplyr::filter(temp, temp$ID_ESTADO == i )
-    B2 <- dplyr::filter(LINEGI, LINEGI$CVE_ENT== unique(LINEGI$CVE_ENT)[i])
-    print(unique(LINEGI$CVE_ENT)[i])
-    MunicipiosMapa<-rbind(MunicipiosMapa,left_join(B2,A2, by = "MunMin")) #INEGI a la izquierda
-  }
-  
-  8. en funcion de mapas municipales (Para secciones electorales) añadir el nombre del Municipios
+  8. en funcion de mapas municipales (Para secciones electorales) añadir el nombre de Municipios
   9. ENOE a nivel municipal
   10. Variables a nivel municipal de la intercensal 2015 
   #11. Jalisco, unir municipios por nombre
@@ -25,4 +16,11 @@ Por hacer:
   15. Revisar como mantener las clases a la hora de hacer mapas
   16. Establecer temática de colores para mapas
   17. Programar función para etiquetas de los mapas 
-  18. 
+  18. Mapas con secciones electorales
+  19. Lectura y orden de la ENOE
+  20. Desempeño por variables
+  21. Intercensal por Municipio
+  22. Variación de ingreso, mujeres grupos de edad Jalisco, corte urbano, rural.  
+  23. Cambio en informalidad, mujeres, por edad
+  24. Puntos, antes de x elección, último periodo observable, etc. 
+  25. Varias ENOEs, tamaños de muestra 
