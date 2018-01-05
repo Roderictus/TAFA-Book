10b70be6e2c634d9e4a34bfae2af87a344e870a2
RODRIGO FRANCO <rodrigofrancofuentes@gmail.com>
2018-01-05 16:56:16 -0600

    orden y revision

--- Bases.R
+++ Bases.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(readxl)
 library(stringr)
+library(foreign)
 library(data.table)
 #Bases de datos
 #Archivo para construir las bases de datos, se sustituye la inclusión de las bases en el cuerpo del documento con un
@@ -12,7 +13,7 @@ library(data.table)
 #######################################################################################################
 
 P2006Secc<-fread(input = "http://siceef.ine.mx/BD/Presidente2006Seccion.csv", 
-                 sep = ",", encoding = "UTF-8")q3
+                 sep = ",", encoding = "UTF-8")
 P2006Secc$CVE_SECC <-str_c(str_pad(P2006Secc$ID_ESTADO, width =2, "left", "0"),         #Estado Sección
                            str_pad(P2006Secc$SECCION, width = 4, "left", "0"))
 P2006Secc$CV_MUN   <- str_c(str_pad(P2006Secc$ID_ESTADO, width = 2, "left", "0"),       #Estado Municipio
@@ -21,7 +22,7 @@ P2006Secc$CV_TODO  <- str_c(str_c(str_pad(P2006Secc$ID_ESTADO, width = 2, "left"
                                   str_pad(P2006Secc$ID_MUNICIPIO, width = 3, "left", "0")), 
                             str_pad(P2006Secc$SECCION, width = 4, "left", "0"))
 P2006Secc<-filter(P2006Secc, MUNICIPIO != "Voto en el Extranjero")
-write.csv(x = P2006Secc, file = "Datos/Electorales/P2006Secc.csv")# N
+write.csv(x = P2006Secc, file = "Datos/Electorales/P2006Secc.csv")
 
 ####################      P2006 Mun      ##############################################################
 P2006Mun<-P2006Secc %>% #2442 Municipios 
@@ -39,7 +40,8 @@ P2006Mun<-P2006Secc %>% #2442 Municipios
             Num_Casillas  = length(unique(na.omit(CASILLA))),
             Municipio     = unique(MUNICIPIO), 
             ID_Municipio  = unique(ID_MUNICIPIO))
-P2006Mun$CVE_ENT    <- str_sub(string = P2006Mun$CVUN, start = 1, end = 2)
+P2006Mun$CV_MUN <- str_c(str_pad(P2006Mun$ID_ESTADO, width = 2, "left", "0"),       #Estado Municipio
+                            str_pad(P2006Mun$ID_MUNICIPIO, width = 3, "left", "0"))
 P2006Mun$Por_Part   <- (P2006Mun$TOTAL / P2006Mun$Lista_Nominal) * 100 
 P2006Mun$PAN_por    <- (P2006Mun$PAN/P2006Mun$TOTAL) * 100
 P2006Mun$APM_por    <- (P2006Mun$APM/P2006Mun$TOTAL) * 100
@@ -68,7 +70,7 @@ P2006Edo$APM_por <- (P2006Edo$APM/P2006Edo$TOTAL) * 100
 P2006Edo$PBT_por <- (P2006Edo$PBT/P2006Edo$TOTAL) * 100
 P2006Edo$NVA_ALIANZA_por <- (P2006Edo$NVA_ALIANZA/P2006Edo$TOTAL) * 100
 P2006Edo$ASDC_por <- (P2006Edo$ASDC/P2006Edo$TOTAL) * 100
-write.csv(x = P2006Edo, file = "Datos/Electorales/P2006Edo.csv")# N
+write.csv(x = P2006Edo, file = "Datos/Electorales/P2006Edo.csv")
 
 ################################################################################################
 #######################################       2012  SECC    ########################################
@@ -106,7 +108,21 @@ P2012Mun<-P2012Secc %>%   #2448
             NULOS         = sum(NUM_VOTOS_NULOS, na.rm = TRUE),
             Num_Secciones = length(unique(na.omit(SECCION))),
             Num_Casillas  = length(unique(na.omit(CASILLAS))))
+P2012Mun$Por_Part   <- (P2012Mun$TOTAL / P2012Mun$Lista_Nominal) * 100  
+P2012Mun$PAN_por         <- (P2012Mun$PAN/P2012Mun$TOTAL)  * 100
+P2012Mun$PRI_por         <- (P2012Mun$PRI/P2012Mun$TOTAL)  * 100
+P2012Mun$PRD_por         <- (P2012Mun$PRD/P2012Mun$TOTAL)  * 100
+P2012Mun$PVEM_por        <- (P2012Mun$PVEM/P2012Mun$TOTAL) * 100
+P2012Mun$PT_por          <- (P2012Mun$PT/P2012Mun$TOTAL)   * 100
+P2012Mun$MC_por          <- (P2012Mun$MC/P2012Mun$TOTAL)   * 100
+P2012Mun$NVA_ALIANZA_por <- (P2012Mun$NVA_ALIANZA/P2012Mun$TOTAL)   * 100
+P2012Mun$PRI_PVEM_por    <- (P2012Mun$PRI_PVEM/P2012Mun$TOTAL)   * 100
+P2012Mun$PRD_PT_MC_por   <- (P2012Mun$PRD_PT_MC/P2012Mun$TOTAL)   * 100
+P2012Mun$PRD_PT_por      <- (P2012Mun$PRD_PT/P2012Mun$TOTAL)   * 100
+P2012Mun$PRD_MC_por      <- (P2012Mun$PRD_MC/P2012Mun$TOTAL)   * 100
+P2012Mun$PT_MC_por       <- (P2012Mun$PT_MC/P2012Mun$TOTAL)   * 100
 write.csv(x = P2012Mun, file = "Datos/Electorales/P2012Mun.csv")# N
+
 ###########################   Datos 2012 Estatal  #########################################
 P2012Edo<- P2012Mun %>% group_by(NOMBRE_ESTADO, ID_ESTADO) %>% 
   summarise(TOTAL = sum(TOTAL, na.rm = TRUE),
@@ -180,14 +196,9 @@ JALSECCDIP2015 <- JALSECC2015 %>% filter(Elección == "Diputados MR")
 
 ###################################         Jalisco a Nivel Municipal       ################################
 #JALSECCMUN2015 <-fread(file = "Datos/Electorales/Jalisco/JALSECCMUN2015.csv")
-
 #hay 10 secciones electorales, en el municipio de Guadalajara con nas en Votos Validos
 #Asignamos Votos Totales a los votos validos
-
 JALSECCMUN2015[is.na(JALSECCMUN2015$VotosValidos),]$VotosValidos <-JALSECCMUN2015[is.na(JALSECCMUN2015$VotosValidos),]$VotosTotales
-
-head(JALSECCMUN2015)# 7161
-
 
 JALMUNMUN2015 <- JALSECCMUN2015 %>%      #municipes en municipios, 125 Municipios
   group_by(Municipio) %>%
@@ -203,12 +214,6 @@ JALMUNMUN2015 <- JALSECCMUN2015 %>%      #municipes en municipios, 125 Municipio
             Boletas = sum(Boletas),
             VotosTotales = sum(VotosTotales), 
             VotosValidos = sum(VotosValidos))
-
-write.csv(x = JALSECCMUN2015, file = "Datos/Electorales/Jalisco/JALSECCMUN2015.csv")
-write.csv(x = JALSECCDIP2015, file = "Datos/Electorales/Jalisco/JALSECCDIP2015.csv")
-
-#Base de datos a nivel municipal
-
 JALMUNMUN2015<- JALMUNMUN2015 %>% 
   mutate(Por_Part  = (VotosTotales/Boletas)*100,
          PAN_por    = (PAN/VotosValidos)*100,
@@ -222,10 +227,77 @@ JALMUNMUN2015<- JALMUNMUN2015 %>%
          JPK_por    = (JPK/VotosValidos)*100, 
          Boletas_Por= (Boletas/sum(Boletas)*100),#porcentaje de la lista nominal que tiene cada municipio
          Validos_Por= (VotosValidos/ sum(VotosValidos)*100))#porcentaje de la votación efectiva de cada municipio 
+head(JALMUNMUN2015)
+
+write.csv(x = JALMUNMUN2015, file = "Datos/Electorales/Jalisco/JALMUNMUN2015.csv")
+write.csv(x = JALSECCMUN2015, file = "Datos/Electorales/Jalisco/JALSECCMUN2015.csv")
+write.csv(x = JALSECCDIP2015, file = "Datos/Electorales/Jalisco/JALSECCDIP2015.csv")
+
+#####################################################################################################
+##############      ENOE a nivel municipal      ####################################################
+#####################################################################################################
+# De sdem105.dbf a sdem416.dbf
+ENOE <- read.dbf(file = "Datos/ENOE/Sociodemografico/sdemt317.dbf") #  3er trimestre 2017 
+ENOE <- filter(ENOE, R_DEF == "00") #00 Resultado definitivo de la entrevista, entrevista completa
+ENOE$EDA<-as.numeric(ENOE$EDA)
+ENOE <- filter(ENOE, EDA >= 14)
+ENOE <- filter(ENOE, EDA <=98)
+ENOE <- filter(ENOE, C_RES == 1 | C_RES == 3) #Condiciónn de la residencia, 1 Residente Habitual, 3 Nuevo residente 
+# Nuevas variables
+ENOE<- mutate(ENOE, INF = ifelse(TUE2==5,1,0))
+ENOE<- mutate(ENOE, INF_POB = INF * FAC)
+ENOE<- mutate(ENOE, Estado = as.factor(ENT))
+#nombres de los Estados
+ENOE$Estado<- factor(ENOE$Estado, labels = c("AGS", "BC", "BCS","CAMP", "COAH", "COL", "CHIS", "CHIH", "DF", "DGO", "GTO","GRO", "HGO", "JAL", "MEX","MICH", "MOR", "NAY", "NL", "OAX","PUE", "QRO", "QR", "SLP", "SIN", "SON", "TAB", "TAMS", "TLAX", "VER", "YUC", "ZAC"))
+ENOE$SEX<- factor(ENOE$SEX, labels = c("HOMBRE","MUJER")) #Sexo
+ENOE$TUE2<- factor(ENOE$TUE2, labels = c("No Clasif","EmpSocCop","Neg. noConst", "Priv", "Pub", "Sect. Informal", "TDR", "AgSubs")) 
+ENOE$RAMA_EST2<- factor(ENOE$RAMA_EST2, labels = c("No Clasif","Agricultura GSCP","Extractiva y Electricidad", "Manufactura", "Construcción", "Comercio", "Restaurantes y ServAloj", "Transportes y Com...", "Serv. Prof", "Serv. Sociales", "Sevr. Diversos", "Gobierno y Org. Int.")) 
+ENOE$E_CON <- factor(ENOE$E_CON, labels = c ("Unión libre", "Separado(a)", "Divorciado(a)", "Viudo(a)", "Casado(a)", "Soltero(a)", "No sabe"))
+ENOE$CLASE1 <- factor(ENOE$CLASE1, labels = c( "PEA", "No PEA" ))
+ENOE$ING7C<- factor(ENOE$ING7C, labels =c("No Clas","Hasta 1 SM", "1-2 SM", "2-3 SM", "3-5 SM", "Más de 5 SM", "No recibe ingresos", "No especificado"))  #clasificación de la población ocupada por nivel de ingreso 
+ENOE$MEDICA5C <- factor(ENOE$MEDICA5C, labels = c("No Clas", "Sin prestaciones", "Solo acceso a instituciones de salud", "Acceso a inst. de salud y otras prest.", "no tiene acceso, pero si otras prestaciones", "No especificado" ))
+ENOE$AMBITO2 <- factor (ENOE$AMBITO2, labels = c("no clas", "Sin Establecimiento" , "Con establecimiento", "Pequeños establecimientos" , "Medianos Establecimientos", "Grandes Establecimientos", "Gobierno", "Otros") )
+ENOE<- mutate(ENOE, Mujer = ifelse(SEX == "MUJER",1,0)) #Mujeres
+ENOE<- mutate(ENOE, Mujer_Pob = FAC * Mujer) #Mujeres multiplicadas por el factor de poblacion
+ENOE<- mutate(ENOE, Hombre = ifelse(SEX == "HOMBRE",1,0)) #Hombres
+ENOE<- mutate(ENOE, Hombre_Pob = FAC * Hombre) #Hombres multiplicados por el factor de poblacion
+ENOE<- mutate(ENOE, ING_Pob_HR = ING_X_HRS * FAC)
+ENOE<- mutate(ENOE, OCUPADA = ifelse(CLASE2 ==1,1,0)) #Poblacion ocupada, para sacar ingresos promedio
+ENOE<- mutate(ENOE, OCUPADA_Pob = OCUPADA * FAC) #Poblacion ocupada, para sacar ingresos promedio
+ENOE<- mutate(ENOE, HRSOCUP_Pob = HRSOCUP * FAC)
+ENOE<- mutate(ENOE, ANIOS_ESC_Pob = ANIOS_ESC * FAC)
+ENOE<- mutate(ENOE, SUB_O_POB = SUB_O * FAC)
+#Codificación de las variables de escolaridad
+ENOE$CS_P13_1<- factor(ENOE$CS_P13_1, labels = c("Ninguna", "Preescolar", "Primaria", "Secundaria","Preparatoria o bach", "Normal", "Carrera técnica", "Profesional", "Maestría", "Doctorado", "No sabe"))
+#ENOE$NS<-ENOE$NS * ENOE$FAC
+write.csv(x = ENOE,file = "Datos/ENOE/Sociodemografico/Formateados CSV/Sdem317.csv")
+
+#####################################################################################################
+##############      Intercensal, Jalisco     ########################################################
+#####################################################################################################
+#sacarlo para Jalisco, municipal, homologarlo con shapefiles. 
+PersonaIC      <- read.csv(file = "C:/Proyectos R/Datos intercensal/Datos Intercensal/TR_PERSONA14.CSV")#persona intercensal, jalisco 
+PersonaIC$SEXO <-factor(PersonaIC$SEXO, labels = c("Hombre", "Mujer"))
+ViviendaIC     <- read.csv(file = "C:/Proyectos R/Datos intercensal/Datos Intercensal/TR_VIVIENDA14.CSV")#Vivienda intercensal, jalisco 
+#la codificación de las etiquetas se obtiene del cuestionario 
+#http://www.beta.inegi.org.mx/contenidos/proyectos/enchogares/especiales/intercensal/2015/doc/eic2015_cuestionario.pdf
+ViviendaIC$JEFE_SEXO<-factor(ViviendaIC$JEFE_SEXO, labels = c("Hombre", "Mujer"))
+ViviendaIC[ViviendaIC$JEFE_EDAD == "999", ]$JEFE_EDAD <- NA#clasificar correctamente los NAs de edad del jefe del hogar
+ViviendaIC %>% filter(JEFE_EDAD != "NA") %>% group_by(MUN, NOM_MUN, JEFE_SEXO ) %>% summarise(EDAD_JEFE_PROMEDIO = weighted.mean(JEFE_EDAD, FACTOR)) #para sacar las promedios ponderados, corte municipio y sexo 
+ViviendaIC %>% filter(INGTRHOG != c("NA", "999999")) %>% group_by(MUN, NOM_MUN, JEFE_SEXO ) %>% summarise(INGRESO_VIVIENDA_PROMEDIO = weighted.mean(JEFE_EDAD, FACTOR), weighted.mean(Ingreso_Hogar, FACTOR)) #para sacar las promedios ponderados, corte municipio y sexo 
+
+#1. Dos bases, personas y vivienda, sacar información a nivel municipal
+#2. Unir datos de las dos bases
+#3. Unir con base broomificada para mapas 
+write.csv(x = PersonaIC, file = "Datos/Intercensal/Persona_2015.csv")# Persona intercensal 
+
+write.csv(x = ViviendaIC, file = "Datos/Intercensal/Vivienda_2015.csv")# vivienda intercensal 
 
 
-#Bases de construcción cartográfica
 
+##########################################################################
+################      Bases de construcción cartográfica      ############
+##########################################################################
 Municipios <-readOGR("C:/Proyectos R/Analisis Electoral Puebla/Datos/Cartográficos/Municipios Nacional/areas_geoestadisticas_municipales.shp", "areas_geoestadisticas_municipales", encoding = "UTF8") #cartografía de Jalisco 14
 MunMapJal15 <- Municipios[Municipios@data$CVE_ENT == "14",] #Subseteo de los datos geográficos del estado que nos interesa 
 rm(Municipios)#WastenotWantNot RAM
@@ -265,68 +337,9 @@ MunMapJal2015<-left_join(x = MunMapJal, y = JALMUNMUN2015, by = "NOM_MUN_JOIN")#
 write.csv(x = MunMapJal2015, file = "Datos/Electorales/Jalisco/MunMapJal2015.csv")
 write.csv(x = JALMUNMUN2015, file = "Datos/Electorales/Jalisco/JALMUNMUN2015.csv")
 
-#####################################################################################################
-##############      ENOE a nivel municipal      ####################################################
-#####################################################################################################
-
-# De sdem105.dbf a sdem416.dbf
-ENOE <- read.dbf(file = "Datos/ENOE/Sociodemografico/sdemt317.dbf") #  2do trimestre 2015 
-ENOE <- filter(ENOE, R_DEF == "00") #00 Resultado definitivo de la entrevista, entrevista completa
-ENOE$EDA<-as.numeric(ENOE$EDA)
-ENOE <- filter(ENOE, EDA >= 14)
-ENOE <- filter(ENOE, EDA <=98)
-ENOE <- filter(ENOE, C_RES == 1 | C_RES == 3) #Condiciónn de la residencia, 1 Residente Habitual, 3 Nuevo residente 
-# Nuevas variables
-ENOE<- mutate(ENOE, INF = ifelse(TUE2==5,1,0))
-ENOE<- mutate(ENOE, INF_POB = INF * FAC)
-ENOE<- mutate(ENOE, Estado = as.factor(ENT))
-#nombres de los Estados
-ENOE$Estado<- factor(ENOE$Estado, labels = c("AGS", "BC", "BCS","CAMP", "COAH", "COL", "CHIS", "CHIH", "DF", "DGO", "GTO","GRO", "HGO", "JAL", "MEX","MICH", "MOR", "NAY", "NL", "OAX","PUE", "QRO", "QR", "SLP", "SIN", "SON", "TAB", "TAMS", "TLAX", "VER", "YUC", "ZAC"))
-ENOE$SEX<- factor(ENOE$SEX, labels = c("HOMBRE","MUJER")) #Sexo
-ENOE$TUE2<- factor(ENOE$TUE2, labels = c("No Clasif","EmpSocCop","Neg. noConst", "Priv", "Pub", "Sect. Informal", "TDR", "AgSubs")) 
-ENOE$RAMA_EST2<- factor(ENOE$RAMA_EST2, labels = c("No Clasif","Agricultura GSCP","Extractiva y Electricidad", "Manufactura", "Construcción", "Comercio", "Restaurantes y ServAloj", "Transportes y Com...", "Serv. Prof", "Serv. Sociales", "Sevr. Diversos", "Gobierno y Org. Int.")) 
-ENOE$E_CON <- factor(ENOE$E_CON, labels = c ("Unión libre", "Separado(a)", "Divorciado(a)", "Viudo(a)", "Casado(a)", "Soltero(a)", "No sabe"))
-ENOE$CLASE1 <- factor(ENOE$CLASE1, labels = c( "PEA", "No PEA" ))
-ENOE$ING7C<- factor(ENOE$ING7C, labels =c("No Clas","Hasta 1 SM", "1-2 SM", "2-3 SM", "3-5 SM", "Más de 5 SM", "No recibe ingresos", "No especificado"))  #clasificación de la población ocupada por nivel de ingreso 
-ENOE$MEDICA5C <- factor(ENOE$MEDICA5C, labels = c("No Clas", "Sin prestaciones", "Solo acceso a instituciones de salud", "Acceso a inst. de salud y otras prest.", "no tiene acceso, pero si otras prestaciones", "No especificado" ))
-ENOE$AMBITO2 <- factor (ENOE$AMBITO2, labels = c("no clas", "Sin Establecimiento" , "Con establecimiento", "Pequeños establecimientos" , "Medianos Establecimientos", "Grandes Establecimientos", "Gobierno", "Otros") )
-ENOE<- mutate(ENOE, Mujer = ifelse(SEX == "MUJER",1,0)) #Mujeres
-ENOE<- mutate(ENOE, Mujer_Pob = FAC * Mujer) #Mujeres multiplicadas por el factor de poblacion
-ENOE<- mutate(ENOE, Hombre = ifelse(SEX == "HOMBRE",1,0)) #Hombres
-ENOE<- mutate(ENOE, Hombre_Pob = FAC * Hombre) #Hombres multiplicados por el factor de poblacion
-ENOE<- mutate(ENOE, ING_Pob_HR = ING_X_HRS * FAC)
-ENOE<- mutate(ENOE, OCUPADA = ifelse(CLASE2 ==1,1,0)) #Poblacion ocupada, para sacar ingresos promedio
-ENOE<- mutate(ENOE, OCUPADA_Pob = OCUPADA * FAC) #Poblacion ocupada, para sacar ingresos promedio
-ENOE<- mutate(ENOE, HRSOCUP_Pob = HRSOCUP * FAC)
-ENOE<- mutate(ENOE, ANIOS_ESC_Pob = ANIOS_ESC * FAC)
-ENOE<- mutate(ENOE, SUB_O_POB = SUB_O * FAC)
-#Codificación de las variables de escolaridad
-ENOE$CS_P13_1<- factor(ENOE$CS_P13_1, labels = c("Ninguna", "Preescolar", "Primaria", "Secundaria","Preparatoria o bach", "Normal", "Carrera técnica", "Profesional", "Maestría", "Doctorado", "No sabe"))
-#ENOE$NS<-ENOE$NS * ENOE$FAC
-write.csv(x = ENOE,file = "Datos/ENOE/Sociodemografico/Formateados CSV/Sdem317.csv")
 
 
 #para mujeres por rangos de edad
-#####################################################################################################
-##############      Intercensal, Jalisco     ########################################################
-#####################################################################################################
-#sacarlo para Jalisco, municipal, homologarlo con shapefiles. 
-PersonaIC <- read.csv(file = "C:/Proyectos R/Datos intercensal/Datos Intercensal/TR_PERSONA14.CSV")#persona intercensal, jalisco 
-PersonaIC$SEXO<-factor(PersonaIC$SEXO, labels = c("Hombre", "Mujer"))
-ViviendaIC <- read.csv(file = "C:/Proyectos R/Datos intercensal/Datos Intercensal/TR_VIVIENDA14.CSV")#Vivienda intercensal, jalisco 
-#la codificación de las etiquetas se obtiene del cuestionario 
-#http://www.beta.inegi.org.mx/contenidos/proyectos/enchogares/especiales/intercensal/2015/doc/eic2015_cuestionario.pdf
-ViviendaIC$JEFE_SEXO<-factor(ViviendaIC$JEFE_SEXO, labels = c("Hombre", "Mujer"))
-ViviendaIC[ViviendaIC$JEFE_EDAD == "999", ]$JEFE_EDAD <- NA#clasificar correctamente los NAs de edad del jefe del hogar
-ViviendaIC %>% filter(JEFE_EDAD != "NA") %>% group_by(MUN, NOM_MUN, JEFE_SEXO ) %>% summarise(EDAD_JEFE_PROMEDIO = weighted.mean(JEFE_EDAD, FACTOR)) #para sacar las promedios ponderados, corte municipio y sexo 
-ViviendaIC %>% filter(INGTRHOG != c("NA", "999999")) %>% group_by(MUN, NOM_MUN, JEFE_SEXO ) %>% summarise(INGRESO_VIVIENDA_PROMEDIO = weighted.mean(JEFE_EDAD, FACTOR), weighted.mean(Ingreso_Hogar, FACTOR)) #para sacar las promedios ponderados, corte municipio y sexo 
-
-#1. Dos bases, personas y vivienda, sacar información a nivel municipal
-#2. Unir datos de las dos bases
-#3. Unir con base broomificada para mapas 
-write.csv(x = PersonaIC, file = "Datos/Intercensal/Persona_2015.csv")# Persona intercensal 
-
-write.csv(x = ViviendaIC, file = "Datos/Intercensal/Vivienda_2015.csv")# vivienda intercensal 
 ############################################################################################
 ##########################    Base para Mapas, Intercensal 2015   #########################
 ############################################################################################
